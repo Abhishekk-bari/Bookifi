ccecc1df04e15b4ae34782758607dca0f55acb97
Abhi <abhi2bari929@gmail.com>
2024-12-06 14:06:21 +0530

    not extra just checking

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
-  styleUrl: './review.component.css'
+  styleUrls: ['./review.component.css']
 })
-export class ReviewComponent {
-
+export class ReviewComponent  {
+  
 }
